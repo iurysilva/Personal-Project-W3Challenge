bdf76ee7c44849df163138fbb5db2a9149db7352
iurysilva <iuryglabson18@gmail.com>
2022-11-14 18:45:53 -0300

    Feat: Make logs for login

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,9 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CheckingAccount from 'App/Models/CheckingAccount'
 import Hash from '@ioc:Adonis/Core/Hash'
+import Log from 'App/Models/Log'
+import { DateTime } from 'luxon'
 
 
 export default class AuthController{
+    public async saveLoginLog(checkingAccount: CheckingAccount){
+        await checkingAccount.load('client')
+        await checkingAccount.load('agency')
+        Log.create({
+            title: `Authentication realized by ${checkingAccount.client.name} at ${DateTime.now()}`,
+            description: `The checking account ${checkingAccount.account_number} of agency ${checkingAccount.agency.number} was accessed`
+        })
+    }
+    
     public async login({ auth, request, response }: HttpContextContract){
         const agencyNumber = request.input('agency')
         const checkingAccountNumber = request.input('checking_account')
@@ -24,6 +35,7 @@ export default class AuthController{
             const token = await auth.use('checkingAccount').generate(checkingAccount, {
                 expiresIn: '2days'
                 })
+            this.saveLoginLog(checkingAccount)
             return token
         }else{
             return response.notFound({'message':'User not found'})
